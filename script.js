7b1ceec1bb0280e0e2a901e6a94840eef03041e1
brenbil112 <128422728+brenbil112@users.noreply.github.com>
2025-03-01 21:44:21 -0500

    fixed code

--- script.js
+++ script.js
@@ -25,10 +25,10 @@ notesContainer.addEventListener('click', function(e) {
     else if(e.target.tagName === 'P') {
         notes = document.querySelectorAll(".input-box");
         notes.forEach(nt => {
-            nt.onkeyup  = function() {
-                updateStorage();
+            nt.onkeyup = function() {
+            updateStorage();
             }
-        })
+        });
     }
 });
 
@@ -45,35 +45,3 @@ showNotes();
 function updateStorage() {
     localStorage.setItem('notes', notesContainer.innerHTML);
 }
-
-createBtn.addEventListener('click', () => {
-    let inputBox = document.createElement('p');
-    let img = document.createElement('img');
-    inputBox.className = 'input-box';
-    inputBox.setAttribute('contenteditable', 'true');
-    img.src = "images/delete.png";
-    notesContainer.appendChild(inputBox).appendChild(img);
-});
-
-notesContainer.addEventListener('click', function(e) {
-    if(e.target.tagName === 'IMG') {
-        e.target.parentElement.remove();
-        updateStorage();
-    }
-    else if(e.target.tagName === 'P') {
-        notes = document.querySelectorAll(".input-box");
-        notes.forEach(nt => {
-            nt.onkeyup  = function() {
-                updateStorage();
-            }
-        })
-    }
-});
-
-document.addEventListener("keydown", event => {
-    if(event.key === "Enter") {
-        document.execCommand('insertLineBreak');
-        event.preventDefault();
-        updateStorage();
-    }
-})
