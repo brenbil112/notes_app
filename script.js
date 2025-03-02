06c29cdc02e347ebfa7376fc5d0b51399116a85b
brenbil112 <128422728+brenbil112@users.noreply.github.com>
2025-03-01 21:40:06 -0500

    moved showNotes

--- script.js
+++ script.js
@@ -7,6 +7,39 @@ function showNotes() {
     updateStorage();
 }
 
+createBtn.addEventListener('click', () => {
+    let inputBox = document.createElement('p');
+    let img = document.createElement('img');
+    inputBox.className = 'input-box';
+    inputBox.setAttribute('contenteditable', 'true');
+    img.src = "images/delete.png";
+    notesContainer.appendChild(inputBox).appendChild(img);
+    updateStorage();
+});
+
+notesContainer.addEventListener('click', function(e) {
+    if(e.target.tagName === 'IMG') {
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if(e.target.tagName === 'P') {
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup  = function() {
+                updateStorage();
+            }
+        })
+    }
+});
+
+document.addEventListener("keydown", event => {
+    if(event.key === "Enter") {
+        document.execCommand('insertLineBreak');
+        event.preventDefault();
+        updateStorage();
+    }
+})
+
 showNotes();
 
 function updateStorage() {
